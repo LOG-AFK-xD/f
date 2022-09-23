6b4b936ae130989d71f06b70fdb78ae4389287b6
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-09-23 12:39:55 +0530

    Update pluginshelper.py

--- HyperMenRobot/pyro/pluginshelper.py
+++ HyperMenRobot/pyro/pluginshelper.py
@@ -12,8 +12,8 @@ from pyrogram import Client
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Chat, Message, User
 
-from mrjoker import OWNER_ID, SUPPORT_CHAT
-from mrjoker import pbot
+from HyPerMenRobot import OWNER_ID, SUPPORT_CHAT
+from HyPerMenRobot import pbot
 
 
 def get_user(message: Message, text: str) -> [int, str, None]:
