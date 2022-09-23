68de7fefc2fe8379206c2b319ec3f47cd4a68519
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-09-23 17:50:31 +0530

    Update chatstatus.py

--- HyperMenRobot/modules/helper_funcs/telethn/chatstatus.py
+++ HyperMenRobot/modules/helper_funcs/telethn/chatstatus.py
@@ -1,5 +1,5 @@
-from mrjoker.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
-from mrjoker import DRAGONS
+from HyPerMenRobot.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
+from HyPerMenRobot import DRAGONS
 from telethon.tl.types import ChannelParticipantsAdmins
 
 
