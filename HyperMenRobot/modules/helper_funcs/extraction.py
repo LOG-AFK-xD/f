abada43ecb0360edfc1a0f770ac801c062cbe1c9
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-09-24 09:00:40 +0530

    Update extraction.py

--- HyperMenRobot/modules/helper_funcs/extraction.py
+++ HyperMenRobot/modules/helper_funcs/extraction.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
-from mrjoker import LOGGER
-from mrjoker.modules.user import get_user_id
+from HyPerMenRobot import LOGGER
+from HyPerMenRobot.modules.user import get_user_id
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
