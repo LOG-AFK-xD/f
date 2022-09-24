75d2106ce7e0065de96ecbb3e46a6cb6dadb0d51
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-09-24 19:44:17 +0530

    Update rules_sql.py

--- HyperMenRobot/modules/sql/rules_sql.py
+++ HyperMenRobot/modules/sql/rules_sql.py
@@ -1,6 +1,6 @@
 import threading
 
-from mrjoker.modules.sql import BASE, SESSION
+from HyPerMenRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Column, String, UnicodeText, distinct, func
 
 
