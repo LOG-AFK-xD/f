b50dfe1ddf6eba56c3e64ddbc5a21d64d34b364c
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-09-24 09:51:59 +0530

    Update chats_sql.py

--- HyperMenRobot/modules/sql/chats_sql.py
+++ HyperMenRobot/modules/sql/chats_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
-from mrjoker.modules.sql import BASE, SESSION
+from HyPerMenRobot.modules.sql import BASE, SESSION
 
 
 class Nightmode(BASE):
