0a17b0c90325d810d886e5189a37f87faf4b66b0
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-09-24 19:28:38 +0530

    Update last_fm_sql.py

--- HyperMenRobot/modules/sql/last_fm_sql.py
+++ HyperMenRobot/modules/sql/last_fm_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String
 
-from mrjoker.modules.sql import BASE, SESSION
+from HyPerMenRobot.modules.sql import BASE, SESSION
 
 
 class LastFMUsers(BASE):
