34b4f03fd6fccd752e428c8c8435824204f9bbae
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-09-24 10:11:35 +0530

    Update feds_sql.py

--- HyperMenRobot/modules/sql/feds_sql.py
+++ HyperMenRobot/modules/sql/feds_sql.py
@@ -1,7 +1,7 @@
 import threading
 
-from mrjoker import dispatcher
-from mrjoker.modules.sql import BASE, SESSION
+from HyPerMenRobot import dispatcher
+from HyPerMenRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
 from telegram.error import BadRequest, Unauthorized
 
