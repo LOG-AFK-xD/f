df47ae386e5fcaf5a7bb499396166a7774cf05a6
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-09-25 10:41:41 +0530

    Update get_common_chats.py

--- HyperMenRobot/modules/get_common_chats.py
+++ HyperMenRobot/modules/get_common_chats.py
@@ -6,9 +6,9 @@ from telegram.error import BadRequest, RetryAfter, Unauthorized
 from telegram.ext import CallbackContext, CommandHandler, Filters
 from telegram.ext.dispatcher import run_async
 
-from mrjoker import OWNER_ID, dispatcher
-from mrjoker.modules.helper_funcs.extraction import extract_user
-from mrjoker.modules.sql.users_sql import get_user_com_chats
+from HyPerMenRobot import OWNER_ID, dispatcher
+from HyPerMenRobot.modules.helper_funcs.extraction import extract_user
+from HyPerMenRobot.modules.sql.users_sql import get_user_com_chats
 
 
 @run_async
