96493bb5d83f7d7824de60ae9676281440e3691f
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-09-25 18:49:26 +0530

    Update note.py

--- HyperMenRobot/modules/note.py
+++ HyperMenRobot/modules/note.py
@@ -23,14 +23,14 @@ from telegram.ext import (
 from telegram.ext.dispatcher import run_async
 from telegram.utils.helpers import escape_markdown, mention_markdown
 
-import mrjoker.modules.sql.notes_sql as sql
-from mrjoker import DRAGONS, JOIN_LOGGER, LOGGER, SUPPORT_CHAT, dispatcher
-from mrjoker.modules.disable import DisableAbleCommandHandler
-from mrjoker.modules.helper_funcs.chat_status import connection_status, user_admin
-from mrjoker.modules.helper_funcs.handlers import MessageHandlerChecker
-from mrjoker.modules.helper_funcs.misc import build_keyboard, revert_buttons
-from mrjoker.modules.helper_funcs.msg_types import get_note_type
-from mrjoker.modules.helper_funcs.string_handling import escape_invalid_curly_brackets
+import HyPerMenRobot.modules.sql.notes_sql as sql
+from HyPerMenRobot import DRAGONS, JOIN_LOGGER, LOGGER, SUPPORT_CHAT, dispatcher
+from HyPerMenRobot.modules.disable import DisableAbleCommandHandler
+from HyPerMenRobot.modules.helper_funcs.chat_status import connection_status, user_admin
+from HyPerMenRobot.modules.helper_funcs.handlers import MessageHandlerChecker
+from HyPerMenRobot.modules.helper_funcs.misc import build_keyboard, revert_buttons
+from HyPerMenRobot.modules.helper_funcs.msg_types import get_note_type
+from HyPerMenRobot.modules.helper_funcs.string_handling import escape_invalid_curly_brackets
 
 FILE_MATCHER = re.compile(r"^###file_id(!photo)?###:(.*?)(?:\s|$)")
 STICKER_MATCHER = re.compile(r"^###sticker(!photo)?###:")
