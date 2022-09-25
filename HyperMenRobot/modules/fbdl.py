200cbad2c09159802e3c93f074945f0e39c18c40
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-09-25 08:34:01 +0530

    Update fbdl.py

--- HyperMenRobot/modules/fbdl.py
+++ HyperMenRobot/modules/fbdl.py
@@ -8,8 +8,8 @@ from yt_dlp import YoutubeDL
 from datetime import datetime
 from telethon import events
 
-from mrjoker.utils.plh import is_admin
-from mrjoker import telethn, BOT_USERNAME, SUPPORT_CHAT
+from HyPerMenRobot.utils.plh import is_admin
+from HyPerMenRobot import telethn, BOT_USERNAME, SUPPORT_CHAT
 
 
 def main(url, filename):
