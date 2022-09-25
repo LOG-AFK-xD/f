5de1a4c5de60cb3326eb4f1d63d274fe742a08e1
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-09-25 17:10:35 +0530

    Update tts.py

--- HyperMenRobot/modules/tts.py
+++ HyperMenRobot/modules/tts.py
@@ -6,9 +6,9 @@ from gtts import gTTS
 from telegram import ChatAction
 from telegram.ext import run_async
 
-from mrjoker import dispatcher
-from mrjoker.modules.disable import DisableAbleCommandHandler
-from mrjoker.modules.helper_funcs.alternate import send_action, typing_action
+from HyPerMenRobot import dispatcher
+from HyPerMenRobot.modules.disable import DisableAbleCommandHandler
+from HyPerMenRobot.modules.helper_funcs.alternate import send_action, typing_action
 
 
 @run_async
