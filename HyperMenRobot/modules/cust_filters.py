ac270a361c41f989db75d337061c115881042972
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-09-25 08:21:32 +0530

    Update cust_filters.py

--- HyperMenRobot/modules/cust_filters.py
+++ HyperMenRobot/modules/cust_filters.py
@@ -15,23 +15,23 @@ from telegram.ext import (
 )
 from telegram.utils.helpers import escape_markdown, mention_html
 
-from mrjoker import DRAGONS, LOGGER, dispatcher
-from mrjoker.modules.connection import connected
-from mrjoker.modules.disable import DisableAbleCommandHandler
-from mrjoker.modules.helper_funcs.alternate import send_message, typing_action
-from mrjoker.modules.helper_funcs.chat_status import user_admin
-from mrjoker.modules.helper_funcs.extraction import extract_text
-from mrjoker.modules.helper_funcs.filters import CustomFilters
-from mrjoker.modules.helper_funcs.handlers import MessageHandlerChecker
-from mrjoker.modules.helper_funcs.misc import build_keyboard_parser
-from mrjoker.modules.helper_funcs.msg_types import get_filter_type
-from mrjoker.modules.helper_funcs.string_handling import (
+from HyPerMenRobot import DRAGONS, LOGGER, dispatcher
+from HyPerMenRobot.modules.connection import connected
+from HyPerMenRobot.modules.disable import DisableAbleCommandHandler
+from HyPerMenRobot.modules.helper_funcs.alternate import send_message, typing_action
+from HyPerMenRobot.modules.helper_funcs.chat_status import user_admin
+from HyPerMenRobot.modules.helper_funcs.extraction import extract_text
+from HyPerMenRobot.modules.helper_funcs.filters import CustomFilters
+from HyPerMenRobot.modules.helper_funcs.handlers import MessageHandlerChecker
+from HyPerMenRobot.modules.helper_funcs.misc import build_keyboard_parser
+from HyPerMenRobot.modules.helper_funcs.msg_types import get_filter_type
+from HyPerMenRobot.modules.helper_funcs.string_handling import (
     button_markdown_parser,
     escape_invalid_curly_brackets,
     markdown_to_html,
     split_quotes,
 )
-from mrjoker.modules.sql import cust_filters_sql as sql
+from HyPerMenRobot.modules.sql import cust_filters_sql as sql
 
 HANDLER_GROUP = 10
 
