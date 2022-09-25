5c5459df1efe1ac1d289e685c838f9977cdacb18
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-09-25 11:04:10 +0530

    Update gtranslator.py

--- HyperMenRobot/modules/gtranslator.py
+++ HyperMenRobot/modules/gtranslator.py
@@ -2,8 +2,8 @@ from gpytranslate import SyncTranslator
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext
 
-from mrjoker import dispatcher
-from mrjoker.modules.disable import DisableAbleCommandHandler
+from HyPerMenRobot import dispatcher
+from HyPerMenRobot.modules.disable import DisableAbleCommandHandler
 
 trans = SyncTranslator()
 
@@ -41,7 +41,7 @@ def languages(update: Update, context: CallbackContext) -> None:
     message = update.effective_message
     bot = context.bot
     bot.send_message(
-        text="Click [here](https://t.me/mrjokerloggroup/4) to see the list of supported language codes!",
+        text="Click [here](https://t.me/Team_Bot_SUPPORT) to see the list of supported language codes!",
         chat_id=message.chat.id, disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
 
 __help__ = """ 
